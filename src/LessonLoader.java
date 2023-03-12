91d20e5a9a65bc2f6fe4f26b27402354805d68af
Kyle Jacoby <kyle.jacoby.19@cnu.edu>
2023-03-12 10:31:18 -0400

    implemented (and lightly tested) loading files with MC questions

--- src/LessonLoader.java
+++ src/LessonLoader.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class LessonLoader {
@@ -9,8 +10,9 @@ public class LessonLoader {
     public static List<Lesson> loadLessons() {
         List<Lesson> lessons = new ArrayList<>();
 
-        File[] lessonFiles = new File("../data").listFiles();
+        File[] lessonFiles = new File("data").listFiles();
 
+        int id = 0;
         for (File file : lessonFiles) {
             if (file.isDirectory()) break;
 
@@ -18,11 +20,13 @@ public class LessonLoader {
                 List<Lecture> lectures = new ArrayList<>();
                 List<Question> questions = new ArrayList<>();
 
+
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
 
                     processLine(line, lectures, questions);
                 }
+                lessons.add(new Lesson(questions, lectures, 0, ++id)); // 0 is temp for possible points
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -30,7 +34,7 @@ public class LessonLoader {
 
         }
 
-        return null;
+        return lessons;
     }
 
     private static void processLine(String line, List<Lecture> lectures, List<Question> questions) {
@@ -41,7 +45,7 @@ public class LessonLoader {
             if (!rowScanner.hasNext()) {
                 return;
             }
-            if (rowScanner.next().equals("Lecture")) {
+            if (rowScanner.next().equals("L")) {
                 String lectureText = rowScanner.next();
                 String topic = rowScanner.next();
                 String imageFilePath = rowScanner.next();
@@ -61,8 +65,22 @@ public class LessonLoader {
         switch (questionType) {
             case "MC":
                 // TODO may need try-catch blocks in these
+                MultipleChoiceQuestion q;
+                try {
+                    String prompt = rowScanner.next();
+                    String answer1 = rowScanner.next();
+                    String answer2 = rowScanner.next();
+                    String answer3 = rowScanner.next();
+                    String answer4 = rowScanner.next();
+                    String correctAnswer = rowScanner.next();
+                    String imageFilePath = rowScanner.next();
+                    List<String> answers = new ArrayList<String>(List.of(answer1, answer2, answer3, answer4));
+                    return new MultipleChoiceQuestion(prompt, imageFilePath, answers, correctAnswer);
+                } catch (NoSuchElementException e) {
+                    e.printStackTrace();
+                }
                 // return new MultipleChoiceQuestion
-                break;
+
             case "MA":
                 // TODO
                 // return new MultipleAnswerQuestion
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        boolean login = LoginHelper.login("test@test.com", "test");
+//        boolean login = LoginHelper.login("test@test.com", "test");
+//
+//        System.out.println(login);
 
-        System.out.println(login);
+        List<Lesson> lessons = LessonLoader.loadLessons();
+
+        for (Lesson l : lessons) {
+            System.out.println(l.getCurrQuestion().getPrompt());
+            System.out.println(l.getCurrLecture().getLectureText());
+        }
 
     }
 }
