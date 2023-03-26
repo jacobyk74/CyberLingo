e13c92f484a27173a44821848aab9368e6ed240f
Kyle Jacoby <kyle.jacoby.19@cnu.edu>
2023-03-26 10:30:07 -0400

    designed multiple-choice-question.fxml and related files (controller and css)

---
+++ src/application/MultipleChoiceController.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MultipleChoiceController implements Initializable {
+
+    @FXML
+    private RadioButton answerButton0;
+    private RadioButton answerButton1;
+    private RadioButton answerButton2;
+    private RadioButton answerButton3;
+
+    @FXML
+    private Button backButton;
+    private Button submitButton;
+
+    @FXML
+    private ToggleGroup toggleGroup = new ToggleGroup();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // TODO
+    }
+
+    public void back() {
+        // TODO
+    }
+
+    public void submit() {
+        // TODO
+    }
+
+}
